a73c4756d63d91229d366e12abb6b6c080525e28
Jean Branco Ramos Ccari <jeandynamo098@gmail.com>
2025-05-30 11:54:34 -0500

    add router dom react

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Page from './App'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Match any route */}
+        <Route path="*" element={<Page />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
